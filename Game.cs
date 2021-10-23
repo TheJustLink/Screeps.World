5681f224c18bb322cce3fb29c49b5a6a891c15a6
JustLink <linki_mail@mail.ru>
2021-10-23 14:46:21 +0700

    Sync repository

---
+++ Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Bridge;
+
+namespace Screeps.World
+{
+    public static class Game
+    {
+        public static Action Loop
+        {
+            set => Script.Set("module.exports.loop", value);
+        }
+
+        public static ulong Time => Script.Get<ulong>("Game.time");
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Screeps.World")]
+[assembly: AssemblyDescription("Screeps World API implementation on C#")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Link")]
+[assembly: AssemblyProduct("Screeps.World")]
+[assembly: AssemblyCopyright("Copyright © Link  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("53c1e93f-fcdf-4299-9f80-925d1a81ff68")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
